b4f8b158ffd1dbbfa1c21421bcd3738c2fb5f864
woojo230 <woojunghyck@naver.com>
2026-03-13 18:32:53 +0900

    feat: users 모듈 생성

--- modu-bot-be/src/app.module.ts
+++ modu-bot-be/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 import typeorm from './config/typeorm';
 
 @Module({
@@ -17,6 +18,7 @@ import typeorm from './config/typeorm';
       useFactory: async (configService: ConfigService) =>
         configService.get('typeorm') || {},
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
