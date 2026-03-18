2905d3e5188c0303f0b06698d1ec8635120f6ca9
woojo230 <woojunghyck@naver.com>
2026-03-18 23:58:30 +0900

    refactor: gitignore 설정 추가

--- modu-bot-be/test/test-wallet.js
+++ modu-bot-be/test/test-wallet.js
@@ -2,7 +2,7 @@ const { ethers } = require('ethers');
 
 // 테스트를 위한 임시 주소
 const PRIVATE_KEY =
-  '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef';
+  '9907ea00cc9e912ce18e054016f46e9e4267b98fc2562052267a497048842cf9';
 const wallet = new ethers.Wallet(PRIVATE_KEY);
 
 console.log('=============================');
@@ -11,7 +11,7 @@ console.log(wallet.address);
 console.log('=============================');
 
 // 테스트를 위한 임시 논스값
-const nonce = '285461';
+const nonce = '525691';
 
 // 서명 생성 함수
 async function makeSignature() {
@@ -19,6 +19,7 @@ async function makeSignature() {
 
   console.log('발급된 서명 (포스트맨 /verify 요청용):');
   console.log(signature);
+  console.log('서명 글자수 (0x 포함):', signature.length);
   console.log('=============================');
 }
 
