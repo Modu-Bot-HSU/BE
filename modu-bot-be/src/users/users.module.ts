c00dfbb6e0c524ecfc2c792fec6b5cb8febb524d
woojo230 <woojunghyck@naver.com>
2026-05-14 00:41:48 +0900

    fix: 마이페이지 api 오류 해결

--- modu-bot-be/src/users/users.module.ts
+++ modu-bot-be/src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { Users } from './entities/users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users]), PassportModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
